a428e2fc1ce43a337097e4c05ab66c1b849a3aa9
BaldrProgrammer <boliklevik@gmail.com>
2025-11-13 23:41:56 +0100

    it almost draws

--- UkrainianPaintAnalog/UkrainianPaintAnalog/Form1.cs
+++ UkrainianPaintAnalog/UkrainianPaintAnalog/Form1.cs
@@ -3,12 +3,34 @@ namespace UkrainianPaintAnalog;
 public partial class Form1 : Form
 {
     private Thread CursorSpi;
+    private Point pastPos;
+    private bool isPressed;
+    
     public Form1()
     {
         CursorSpi = new Thread(SpiCursor);
         CursorSpi.IsBackground = true;
         CursorSpi.Start();
         InitializeComponent();
+    }
+
+    private void FillPixel(int x, int y)
+    {
+        if (mainPcBx.Image == null)
+        {
+            mainPcBx.Image = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+
+        using (Graphics g = Graphics.FromImage(mainPcBx.Image))
+        {
+            if (isPressed == false)
+            {
+                g.FillRectangle(Brushes.Black, x, y, 1, 1);
+            }
+            g.DrawLine(Pens.Black, new Point(pastPos.X, pastPos.Y), new Point(x, y));
+        }
+        
+        mainPcBx.Invalidate();
     }
 
     private void SpiCursor()
@@ -21,8 +43,15 @@ public partial class Form1 : Form
                 if (pos.X <= ClientSize.Width && pos.Y <= ClientSize.Height &&  pos.X >= 0 && pos.Y >= 0)
                 {
                     Console.WriteLine(pos);
-                    Thread.Sleep(10);
+                    FillPixel(pos.X, pos.Y);
+                    SpinWait.SpinUntil(() => false, 1);
+                    pastPos = pos;
+                    isPressed = true;
                 }
+            }
+            else
+            {
+                isPressed = false;
             }
         }
     }
--- UkrainianPaintAnalog/UkrainianPaintAnalog/obj/Debug/net10.0-windows/UkrainianPaintAnalog.AssemblyInfo.cs
+++ UkrainianPaintAnalog/UkrainianPaintAnalog/obj/Debug/net10.0-windows/UkrainianPaintAnalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UkrainianPaintAnalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8610c2de44197281760a30d67712e37692d00e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c5bbf79696ec39a8a85d25005627206cecf639")]
 [assembly: System.Reflection.AssemblyProductAttribute("UkrainianPaintAnalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UkrainianPaintAnalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
